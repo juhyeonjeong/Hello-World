6a107fd558a6df7bb86a4f301f50ad2f329a8692
juhyeonjeong <juhyeonjeong@outlook.kr>
2018-06-19 01:22:52 +0900

    Add files via upload
    p.762 ~ 785

---
+++ Scene-Trans01/Scene-Trans01/SecondViewController.swift
@@ -0,0 +1,8 @@
+
+import UIKit
+
+class SecondViewController : UIViewController {
+    
+}
+
+
---
+++ Scene-Trans01/Scene-Trans01/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Scene-Trans01
+//
+//  Created by juhyeonjeong on 2018. 6. 19..
+//  Copyright © 2018년 juhyeonjeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func moveNext(_ sender: Any){
+        let uvc = self.storyboard!.instantiateViewController(withIdentifier: "SecondVC")
+        uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+        self.present(uvc, animated: true)
+    }
+
+
+}
+
